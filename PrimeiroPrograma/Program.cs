d1fb1f51ae5f09669b0ea6f0215ebb5bc1ed8464
Maicon Alexandre <maicon.dasilvaa@outlook.com>
2025-09-24 14:17:46 -0300

    Retornado texto anterior do commit

--- PrimeiroPrograma/Program.cs
+++ PrimeiroPrograma/Program.cs
@@ -13,7 +13,7 @@
 
 void ExibirOpcoesDoMenu()
 {
-    Console.WriteLine("\nDigite TESTE DE RETORNO DE COMMIT1 para registrar banda");
+    Console.WriteLine("\nDigite 1 para registrar banda");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
